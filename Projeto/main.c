b02b0238c3d6ba4b2a4f51d9228f761262c4b324
Fernando Suares <fernandoaqs@outlook.com.br>
2021-11-10 08:41:59 -0300

    inicio projeto final C

---
+++ Projeto/main.c
@@ -0,0 +1,17 @@
+
+
+#include <stdio.h>
+#include <stdlib.h>
+
+void main(){
+    int opcao;
+
+    printf("Digite a opção desejada.\n\n1 - Login\n2 - Cadastro");
+    scanf("%d", &opcao);
+
+    switch(opcao){
+        case 1 : 
+        case 2 :
+    }
+    
+}
